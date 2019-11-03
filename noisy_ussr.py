66666461825e410ed81205deddf0ca02af37b1db
umyomyomyon <MizukiOhshima@ooshimazuiaois-MacBook-Pro.local>
2019-11-04 03:48:33 +0900

    otoware

---
+++ noisy_ussr.py
@@ -0,0 +1,28 @@
+import discord
+import time
+
+TOKEN = 'YOUR TOKEN'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+    print('ログインしました')
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    #USSRが含まれたメンションが飛んできたら処理開始
+    if client.user in message.mentions:
+        if 'USSR' in message.content:
+            target = message.author.voice.channel.id
+            vc = await client.get_channel(target).connect()
+            vc.play(discord.FFmpegPCMAudio('USSR.mp3'))
+            #ここで再生時間を調整
+            time.sleep(13)
+            await vc.disconnect()
+
+client.run(TOKEN)
+            
