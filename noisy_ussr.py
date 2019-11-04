8d322f49d4a7f695e266d2403babd9efffecc9d6
umyomyomyon <MizukiOhshima@ooshimazuiaois-MacBook-Pro.local>
2019-11-05 03:39:43 +0900

    CCCPと音割れポッターに対応

--- noisy_ussr.py
+++ noisy_ussr.py
@@ -16,13 +16,19 @@ async def on_ready():
 async def on_message(message):
     #USSRが含まれたメンションが飛んできたら処理開始
     if client.user in message.mentions:
-        if 'USSR' in message.content:
+        if 'USSR' in message.content or 'CCCP' in message.content:
             target = message.author.voice.channel.id
             vc = await client.get_channel(target).connect()
             vc.play(discord.FFmpegPCMAudio('USSR.mp3'))
             #ここで再生時間を調整
             time.sleep(13)
-            await vc.disconnect()
+        elif '音割れポッター' in message.content:
+            target = message.author.voice.channel.id
+            vc = await client.get_channel(target).connect()
+            vc.play(discord.FFmpegPCMAudio('potter.mp3'))
+            time.sleep(9)
+        
+        await vc.disconnect()
 
 client.run(TOKEN)
             
