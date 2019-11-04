2cf801fba2dacf7861aca105cddde7d1f3c6e50a
umyomyomyon <MizukiOhshima@ooshimazuiaois-MacBook-Pro.local>
2019-11-05 03:56:21 +0900

    dont repeat yourself

--- noisy_ussr.py
+++ noisy_ussr.py
@@ -1,7 +1,7 @@
 import discord
 import time
 
-TOKEN = 'YOUR TOKEN'
+TOKEN = ''
 
 client = discord.Client()
 
@@ -11,22 +11,21 @@ async def on_ready():
     print('ログインしました')
     print('------')
 
-
 @client.event
 async def on_message(message):
     #USSRが含まれたメンションが飛んできたら処理開始
     if client.user in message.mentions:
         if 'USSR' in message.content or 'CCCP' in message.content:
-            target = message.author.voice.channel.id
-            vc = await client.get_channel(target).connect()
-            vc.play(discord.FFmpegPCMAudio('USSR.mp3'))
-            #ここで再生時間を調整
-            time.sleep(13)
+            music = 'USSR.mp3'
+            play_time = 13
         elif '音割れポッター' in message.content:
-            target = message.author.voice.channel.id
-            vc = await client.get_channel(target).connect()
-            vc.play(discord.FFmpegPCMAudio('potter.mp3'))
-            time.sleep(9)
+            music = 'potter.mp3'
+            play_time = 9
+        target = message.author.voice.channel.id
+        vc = await client.get_channel(target).connect()
+        vc.play(discord.FFmpegPCMAudio(music))
+        #ここで再生時間を調整
+        time.sleep(play_time)
         
         await vc.disconnect()
 
